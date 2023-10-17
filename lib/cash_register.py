6f659d9ef817a422ba179a7ec0f9efd79b9d5d61
Kevin Breid <kevinbreid@yahoo.com>
2023-10-17 16:36:14 +0200

    Python OOP practice

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+    def __init__(self, discount = 0, total = 0.0, items = None):
+        self.discount = discount
+        self.total = total
+        if items == None:
+          self.items = []
+        else:
+            self.items = items
+
+    def add_item(self, title, price, quantity = 1):
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+        self.total += (price * quantity)
+    
+    def apply_discount(self):
+        disc_per = float(self.discount) / 100
+        self.total = self.total - (self.total * disc_per)
+        print(f"After the discount, the total comes to ${int(self.total)}.\n")
+        
+        
